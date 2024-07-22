bb6abf0de7bcb4980e231426355147e695558dd0
FredBettecher <fredbettecher@gmail.com>
2024-07-22 19:00:41 -0300

    style: change the under construction page layout

--- src/containers/under-construction/index.jsx
+++ src/containers/under-construction/index.jsx
@@ -1,7 +1,7 @@
 import BackgroundComponent from "@/components/Background";
 import { FaSquareInstagram, FaSquareWhatsapp } from "react-icons/fa6";
 import Image from "next/image";
-import Logo from "../../../public/logo.svg"
+import Logo from "../../../public/logo.webp"
 import Text from "../../../public/images/under-construction/text.png"
 import Link from "next/link";
 
@@ -21,21 +21,14 @@ export default function UnderConstruction() {
         <Image
           src={Logo}
           alt="Mega Show"
-          className="md:w-[80%] md:mt-2 mt-32"
+          className="w-[65%] lg:w-[30%] lg:fixed lg:top-0 lg:left-20"
         />
       </div>
-      <div className="absolute -top-20 md:top-0 left-0 w-full md:w-fit h-dvh flex items-center justify-center overflow-hidden">
-        <Image
-          src={Text}
-          alt="Refrescância que marca momentos"
-          className="md:w-[80%] md:mr-12 md:mt-2 mt-72"
-        />
-      </div>
-      <div className="fixed bottom-4 right-4 flex flex-col space-y-8 z-10 w-fit">
-        <Link href={"https://www.instagram.com/megashowmais/"} target="_blank" rel="noopener noreferrer" className="text-7xl text-[#E4202C] hover:text-[#EB6272] hover:cursor-pointer transition duration-150">
+      <div className="fixed bottom-4 right-0 flex w-full justify-center space-x-10 z-10">
+        <Link href={"https://www.instagram.com/megashowmais/"} target="_blank" rel="noopener noreferrer" className="text-9xl text-[#fff] hover:text-[#c9c9c9] hover:cursor-pointer transition duration-150">
           <FaSquareInstagram />
         </Link>
-        <Link href={"https://wa.me/+5521997118909"} target="_blank" rel="noopener noreferrer" className="text-7xl text-[#5EB030] hover:text-[#92C67C] hover:cursor-pointer">
+        <Link href={"https://wa.me/+5521997118909"} target="_blank" rel="noopener noreferrer" className="text-9xl text-[#fff] hover:text-[#c9c9c9] hover:cursor-pointer">
           <FaSquareWhatsapp />
         </Link>
       </div>
